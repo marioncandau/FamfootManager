c7e5e1cfba5bff76889e49795cf7a665fc139c8e
Marion Candau <71583439+marioncandau@users.noreply.github.com>
2020-10-03 11:38:10 +0200

    ajout de l'api

---
+++ dates.php
@@ -0,0 +1,33 @@
+<?php
+	// Connect to database
+	include("db_connect.php");
+	$request_method = $_SERVER["REQUEST_METHOD"];
+
+	function getDates()
+	{
+		global $conn;
+		$query = "SELECT DISTINCT date FROM matchs";
+		$response = array();
+		$result = mysqli_query($conn, $query);
+		while($row = mysqli_fetch_array($result))
+		{
+			$response[] = $row;
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response, JSON_PRETTY_PRINT);
+	}
+	
+	switch($request_method)
+	{
+		
+		case 'GET':
+			// Retrieve Matchs
+			getDates();
+			break;
+		default:
+			// Invalid Request Method
+			header("HTTP/1.0 405 Method Not Allowed");
+			break;
+
+	}
+?>
